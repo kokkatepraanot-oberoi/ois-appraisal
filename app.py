09ba701e74b1eca76d68e2d1165b011af690a5bb
kokkatepraanot-oberoi <praanot.kokkate@oberoi-is.org>
2025-08-24 10:30:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,8 @@ if "token" in st.session_state and st.session_state["token"]:
         st.session_state.auth_role = role
 
         st.success(f"✅ Welcome {name} ({role}) — redirecting…")
-        st.switch_page("main.py")
+        st.switch_page("OIS Teacher Self-Assessment")
+
 
 
 
