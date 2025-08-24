e24476c6d19140188bef020562114c48ed53ed7f
kokkatepraanot-oberoi <praanot.kokkate@oberoi-is.org>
2025-08-24 10:02:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ if "token" in st.session_state and st.session_state["token"]:
         st.session_state.auth_role = role
 
         st.success(f"✅ Welcome {name} ({role}) — redirecting…")
-        st.switch_page("pages/main.py")
+        st.switch_page("Main")
 
 
     else:
